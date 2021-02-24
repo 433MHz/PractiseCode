2d712c6eb1b731fa55041dd8ee6afe5bc2cb38b0
Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-02-25 00:23:38 +0100

    Annotation based configuration of spring core

--- SpringCore/src/main/java/pl/krystian/SpringCore/App.java
+++ SpringCore/src/main/java/pl/krystian/SpringCore/App.java
@@ -10,7 +10,7 @@ public class App
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
     	
-    	Vehicle obj = (Vehicle) context.getBean("vehicle");
+    	Vehicle obj = (Vehicle) context.getBean("bike");
     	obj.turnOn();
     	obj.drive();
     	obj.turnOff();
--- SpringCore/src/main/java/pl/krystian/SpringCore/Bike.java
+++ SpringCore/src/main/java/pl/krystian/SpringCore/Bike.java
@@ -1,5 +1,8 @@
 package pl.krystian.SpringCore;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Bike implements Vehicle{
 
 	public void drive() {
