f8661747238121310629118db0fba708295eff41
Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-03-05 19:23:40 +0100

    Project created

---
+++ SpringDataV2/src/main/java/pl/krystian/SpringDataV2Application.java
@@ -0,0 +1,13 @@
+package pl.krystian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataV2Application.class, args);
+	}
+
+}
---
+++ SpringDataV2/src/test/java/pl/krystian/SpringDataV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krystian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
