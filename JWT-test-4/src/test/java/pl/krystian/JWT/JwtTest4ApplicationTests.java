93c2826724c3b8643d0f2f49ec11b3a89084c456
Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-03-07 19:24:38 +0100

    Easy example from telusko

---
+++ JWT-test-4/src/test/java/pl/krystian/JWT/JwtTest4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krystian.JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTest4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
